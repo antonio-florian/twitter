63b8270df5301eef7ac04cdd8dc0b47bfb6c34d6
Antonio_Florian <tony@thestudio.com>
2024-05-19 03:05:54 +0300

    Use next-image

--- README.md
+++ README.md
@@ -24,8 +24,8 @@ If you are not familiar with the different technologies used in this project, pl
 -[x] Attach database to UI
 -[x] Add authentication (w/ clerk)
 -[x] Add image upload
--[] "taint" (server-only)
--[] Use Next/Image component
+-[x] "taint" (server-only)
+-[x] Use Next/Image component
 -[] Error management (w/ Sentry)
 -[] Routing/image page (parallel route)
 -[] Delete button (w/ Server Actions)
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,10 +9,16 @@ async function Images() {
   const images = await getMyImages();
 
   return (
-    <div className="flex flex-wrap gap-4">
+    <div className="flex flex-wrap justify-center gap-4">
     {images.map((image) => (
-      <div key = {image.id} className="flex w48 flex-col">
-        <img src ={image.url} alt="image" ></img>
+      <div key = {image.id} className="flex h-48 w-48 flex-col">
+        <Image 
+        src ={image.url} 
+        style={{ objectFit: "contain" }} 
+        alt={image.name} 
+        width={192}
+        height={192}
+        />
         <div>{image.name}</div>
       </div>
     ))}
